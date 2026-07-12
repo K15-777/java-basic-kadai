03890c96da513970557a75cc2d615db5bc98b8df
K15-777 <kizmk15@gmail.com>
2026-07-12 17:45:35 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,13 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		Car_Chapter15 carA = new Car_Chapter15();
+		
+		carA.changeGear(3);
+		carA.run();
+
+	}
+
+}
